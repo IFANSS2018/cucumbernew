a443bd221c9a0372532baabf109f57e4a87af7bb
saralasubramanian <saralasubramanian@yahoo.com>
2019-04-30 12:23:26 +0530

    jenkin

---
+++ src/main/java/cucumberrr/TestRunner2.java
@@ -0,0 +1,20 @@
+package cucumberrr;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions
+/*(features="src/test/resources/cucumbe/AddusingMap.feature",
+glue="cucumberr",
+plugin={"target","json:src/test/resources/JsonReports.json"},
+monochrome=true)*/
+(features = "src/test/resources/cucumbe/",
+glue= {"cucumberrr"},
+plugin = { "html:target", "json:src/main/resources/JsonReport.json"},
+monochrome = true)
+public class TestRunner2 {
+
+}
